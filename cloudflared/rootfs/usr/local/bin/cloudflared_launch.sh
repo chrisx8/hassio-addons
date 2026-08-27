8b3ab33ee8526a697d2d2a102b428f1e6e722f47
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2026-08-27 11:20:25 -0400

    cloudflared config ip version

--- cloudflared/rootfs/usr/local/bin/cloudflared_launch.sh
+++ cloudflared/rootfs/usr/local/bin/cloudflared_launch.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/with-contenv bashio
 
 # Load config
+edge_ip_version=$(bashio::config 'edge_ip_version')
 token=$(bashio::config 'token')
 
 echo "Launching cloudflared..."
-exec /usr/local/bin/cloudflared tunnel --no-autoupdate run --token "$token"
+exec /usr/local/bin/cloudflared tunnel \
+  --edge-ip-version "$edge_ip_version" \
+  --no-autoupdate \
+  run \
+  --token "$token"
